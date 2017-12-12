07f7c1b58c2ccf0d0c007855f9455034ce20f633
gaowei0115 <gao_wei0115@sina.com>
2017-12-12 22:42:11 +0800

    排序算法-冒泡排序

--- data-structure-sort/src/test/java/com/mmc/data/structure/system/test/sort/ArrayBubbleSortTest.java
+++ data-structure-sort/src/test/java/com/mmc/data/structure/system/test/sort/ArrayBubbleSortTest.java
@@ -45,4 +45,46 @@ public class ArrayBubbleSortTest {
         abs.bubbleSort();
         abs.display();
     }
+
+    @Test
+    public void testBubbleSort() {
+        long[] datas = new long[10];
+        // 组织数据
+        datas[0] = 7L;
+        datas[1] = 14L;
+        datas[2] = 2L;
+        datas[3] = 30L;
+        datas[4] = 702L;
+        datas[5] = 120L;
+        datas[6] = 21L;
+        datas[7] = 73L;
+        datas[8] = 1L;
+        datas[9] = 25L;
+        System.out.println("排序之前：");
+        display(datas);
+        long temp ;
+        for (int i = 0; i < datas.length - 1; i++) {
+            for (int j = 0; j < datas.length - i - 1; j++) {
+                if (datas[j] > datas[j + 1]) {
+                    temp = datas[j];
+                    datas[j] = datas[j + 1];
+                    datas[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println("排序之后：");
+        display(datas);
+    }
+
+    public void display(long[] datas) {
+        System.out.print("[");
+        for (int i = 0; i < datas.length; i++) {
+            System.out.print(datas[i]);
+            if (i != datas.length - 1) {
+                System.out.print(",");
+            }
+        }
+        System.out.print("]");
+        System.out.println();
+    }
 }
